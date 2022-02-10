e1f973f43169cfd144441237e04fd81a2ccbab87
jhkwak <>
2022-02-10 12:44:12 +0900

    build 수정

--- project/src/main/java/com/example/sample/repository/OrderRepository.java
+++ project/src/main/java/com/example/sample/repository/OrderRepository.java
@@ -5,5 +5,5 @@ import com.example.sample.domain.entity.OrderInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<OrderInfo, Long>, OrderCustomRepository{
-    ResOrderDetailDTO findByOrderNo(String orderNo);
+
 }
